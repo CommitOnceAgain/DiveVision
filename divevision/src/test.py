ba2ff32b29d7e6284ca6f59e01060f02887e7760
Alexandre <alexandredothennequin@gmail.com>
2024-12-20 18:12:37 +0100

    Add metric in the test routine, and parametrize the method

--- divevision/src/test.py
+++ divevision/src/test.py
@@ -1,10 +1,19 @@
+from typing import Type
+
+import torch
 from divevision.src.datasets.lsui_dataset import LSUIDataset
+from divevision.src.metrics.abstract_metric import AbstractMetric
+from divevision.src.metrics.psnr import PSNRMetric
+from divevision.src.models.abstract_model import AbstractModel
 from divevision.src.models.u_shape_model import UShapeModelWrapper
+from torch.utils.data import Dataset
 
 
-def simple_test_routine() -> None:
-    model = UShapeModelWrapper(training=False)
-    dataset = LSUIDataset(transform=model.preprocessing)
+def simple_test_routine(
+    model: AbstractModel,
+    dataset: Dataset,
+    metric: Type[AbstractMetric],
+) -> None:
 
     input, label = dataset[0]
     output = model.forward(input)
@@ -19,6 +28,14 @@ def simple_test_routine() -> None:
     output_img.save("output.jpg")
     label_img.save("label.jpg")
 
+    val_metric = metric.compute(input, label)
+    print(f"{metric.name} value = {val_metric}")
+
 
 if __name__ == "__main__":
-    simple_test_routine()
+    device = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
+
+    model = UShapeModelWrapper(training=False, device=device)
+    dataset = LSUIDataset(transform=model.preprocessing)
+    metric = PSNRMetric()
+    simple_test_routine(model, dataset, metric)
