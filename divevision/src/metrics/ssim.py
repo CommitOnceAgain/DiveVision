b3963d3f8aa451491c07e88eab434f2129b66de2
Alexandre <alexandredothennequin@gmail.com>
2025-01-09 14:27:29 +0100

    Handle batch in SSIMMetric

--- divevision/src/metrics/ssim.py
+++ divevision/src/metrics/ssim.py
@@ -10,11 +10,23 @@ class SSIMMetric(AbstractMetric):
 
     def compute(self, input: torch.Tensor, target: torch.Tensor) -> torch.Tensor:
         """Compute the SSIM metric between two images"""
+        # Check that both tensors have the same shape
+        assert input.ndim == target.ndim
+
+        # Add batch dimension if they are not batched, for computation compatibility
+        if input.ndim == 3:
+            input = torch.unsqueeze(input, dim=0)
+            target = torch.unsqueeze(target, dim=0)
+
+        # Compute the SSIM metric for pair of item of the batch
         return torch.tensor(
-            ssim(
-                input.detach().cpu().numpy(),
-                target.detach().cpu().numpy(),
-                channel_axis=0,
-                data_range=1,
-            )
+            [
+                ssim(
+                    input[idx].detach().cpu().numpy(),
+                    target[idx].detach().cpu().numpy(),
+                    channel_axis=0,
+                    data_range=1,
+                )
+                for idx in range(input.shape[0])
+            ]
         )
