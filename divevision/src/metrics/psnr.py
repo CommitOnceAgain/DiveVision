96344c18575e526902bd7062efe53c591192fc0f
Alexandre <alexandredothennequin@gmail.com>
2025-01-13 15:17:50 +0100

    PSNR handle batched data

--- divevision/src/metrics/psnr.py
+++ divevision/src/metrics/psnr.py
@@ -9,9 +9,23 @@ class PSNRMetric(AbstractMetric):
     name = "PSNR"
 
     def compute(self, input: torch.Tensor, target: torch.Tensor) -> torch.Tensor:
+        """Compute the Peak Signal-to-Noise Ratio between two images. Handles batched data."""
+        # Check that both tensors have the same shape
+        assert input.ndim == target.ndim
+
+        # Add batch dimension if they are not batched, for computation compatibility
+        if input.ndim == 3:
+            input = torch.unsqueeze(input, dim=0)
+            target = torch.unsqueeze(target, dim=0)
+
+        # Compute the PSNR metric for pair of item of the batch
         return torch.tensor(
-            psnr(
-                input.detach().cpu().numpy(),
-                target.detach().cpu().numpy(),
-            )
+            [
+                psnr(
+                    input[idx].detach().cpu().numpy(),
+                    target[idx].detach().cpu().numpy(),
+                    data_range=1,
+                )
+                for idx in range(input.shape[0])
+            ]
         )
