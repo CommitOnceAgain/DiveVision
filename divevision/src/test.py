1a6764bd81006da6dd7705b528c772611fd1854e
Alexandre <alexandredothennequin@gmail.com>
2025-01-29 16:03:42 +0100

    MLFlow basic usage in the test.py routine

--- divevision/src/test.py
+++ divevision/src/test.py
@@ -1,7 +1,9 @@
 from collections import defaultdict
 import itertools
+from pathlib import Path
 from typing import Type
 
+import numpy as np
 import torch
 from tqdm import tqdm
 from divevision.src.datasets.lsui_dataset import LSUIDataset
@@ -11,6 +13,9 @@ from divevision.src.metrics.ssim import SSIMMetric
 from divevision.src.models.abstract_model import AbstractModel
 from divevision.src.models.u_shape_model import UShapeModelWrapper
 from torch.utils.data import Dataset, DataLoader
+import mlflow
+from dotenv import load_dotenv
+import os
 
 
 def simple_test_routine(
@@ -49,13 +54,31 @@ def simple_test_routine(
                 metrics_val_list[metric.name].append(val_metric.tolist())
 
     # Report all metrics
+    metrics_final = {}
     for metric_name, values in metrics_val_list.items():
         # If batch size > 1, 'metrics' is composed of list of lists, and need to be flattened
         flatten_values = list(itertools.chain.from_iterable(values))
-        print(f"{metric_name} value = {sum(flatten_values) / len(flatten_values)}")
+        metrics_final[metric_name] = np.mean(flatten_values)
+
+    print(metrics_final)
+
+    # Save MLFlow experiment
+    mlflow.set_experiment("Model testing")
+
+    # Create a new MLFlow run
+    with mlflow.start_run():
+        # Log metrics
+        mlflow.log_metrics(metrics_final)
 
 
 if __name__ == "__main__":
+    # Load environment variables from .env file, returns True if at least one environment variable is set
+    assert load_dotenv(Path(".env").resolve())
+
+    mlflow.set_tracking_uri(
+        uri=f"http://{os.environ["MLFLOW_HOST"]}:{os.environ["MLFLOW_PORT"]}"
+    )
+
     device = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
 
     model = UShapeModelWrapper(device=device)
