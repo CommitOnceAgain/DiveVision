03cf255b81b6ad2048d2067d1deb0db8f5ef82ed
Alexandre <alexandredothennequin@gmail.com>
2025-02-11 17:40:37 +0100

    Update tests for the FastAPI app

--- divevision/test/test_app.py
+++ divevision/test/test_app.py
@@ -1,8 +1,7 @@
-from pathlib import Path
+import io
 from fastapi.testclient import TestClient
-import pytest
-from fastapi import FastAPI, Request
-from fastapi.responses import JSONResponse
+from PIL import Image
+import numpy as np
 
 from divevision.src.app.main import app
 
@@ -12,18 +11,14 @@ client = TestClient(app)
 def test_read_main():
     response = client.get("/")
     assert response.status_code == 200
-    assert response.json() == {"message": "Welcome!"}
 
 
 def test_upload_image():
-    prefix = "divevision/test/resources/"
-    filename = "lsui_sample_19.jpg"
-
-    filepath = Path(prefix + filename).resolve()
-    assert filepath.exists() and filepath.is_file()
-
+    random_image = Image.fromarray(np.random.randn(128, 128, 3), mode="RGB")
+    buffer = io.BytesIO()
+    random_image.save(buffer, "PNG")
     response = client.post(
         "/image/",
-        files={"file": (filename, filepath.read_bytes(), "image/jpeg")},
+        files={"file": ("foo.png", buffer.getvalue(), "image/png")},
     )
     assert response.status_code == 200
