ca4a4ebd186cda074548321fc8b2c06d1d183727
Alexandre <alexandredothennequin@gmail.com>
2026-04-15 10:46:48 +0200

    Clean-up and redefine more closely the project

--- divevision/src/test.py
+++ divevision/src/test.py
@@ -142,8 +142,6 @@ def run_benchmark(
 
 
 if __name__ == "__main__":
-    # Load environment variables from .env file, returns True if at least one environment variable is set
-    assert load_dotenv(Path(".env").resolve())
 
     mlflow.set_tracking_uri(
         uri=f"http://{os.environ["MLFLOW_HOST"]}:{os.environ["MLFLOW_PORT"]}"
